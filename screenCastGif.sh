53388a49b9f05004c39d5a64a4248a0ea1d12a0c
Daniele Cruciani <daniele@smartango.com>
2017-09-27 21:04:25 +0200

    target file is on ~/Desktop and does not overwrite

--- screenCastGif.sh
+++ screenCastGif.sh
@@ -11,5 +11,21 @@ INFOS=`xwininfo | awk '/\ \ A/{print $4} /\ \ [WH]/{print $2} '`
 read -r X Y W H lll vo cc <<<$(echo $INFOS)
 echo 'x:' $X 'y:' $Y 'width:' $W 'height:' $H 'time:' ${D}s
 
+function findTarget {
+    local CNT=
+    local TARGET=~/Desktop/screencast${CNT}.gif
 
-byzanz-record -x $X -y $Y -w $W -h $H -d $D /tmp/amazonclic.gif
+    while [ -e $TARGET ]; do
+	if [ "$CNT" == "" ]; then
+	    CNT=0
+	fi
+	CNT=$[CNT+1]
+	TARGET=~/Desktop/screencast_${CNT}.gif
+    done
+    echo $TARGET
+}
+
+TARGET=$(findTarget)
+
+
+byzanz-record -x $X -y $Y -w $W -h $H -d $D $TARGET
